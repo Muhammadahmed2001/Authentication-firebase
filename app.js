5465419c497e1ac9c4d8fb9ba0ae001c0453ef40
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-07-06 19:17:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyDuOrp8JQixQzRW9yojXIWP1EF8U74uCBk",
+    authDomain: "authentication-firebase-e23fb.firebaseapp.com",
+    projectId: "authentication-firebase-e23fb",
+    storageBucket: "authentication-firebase-e23fb.appspot.com",
+    messagingSenderId: "580457386780",
+    appId: "1:580457386780:web:a63da704563d16a3bce0d9"
+};
+const app = initializeApp(firebaseConfig);
