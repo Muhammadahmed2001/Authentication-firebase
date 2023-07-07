078fc94b5d9c373541c05b3efbf54f33007c34e3
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-07-07 23:10:10 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-const firebaseConfig = {
+
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+  import { getAuth , createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+  const firebaseConfig = {
     apiKey: "AIzaSyDuOrp8JQixQzRW9yojXIWP1EF8U74uCBk",
     authDomain: "authentication-firebase-e23fb.firebaseapp.com",
     projectId: "authentication-firebase-e23fb",
     storageBucket: "authentication-firebase-e23fb.appspot.com",
     messagingSenderId: "580457386780",
     appId: "1:580457386780:web:a63da704563d16a3bce0d9"
-};
-const app = initializeApp(firebaseConfig);
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+
+  const auth = getAuth(app);
+
+
+  
+createUserWithEmailAndPassword(auth, "ahmed@gmail.com", "123456")
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user)
+   
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage);
+    
+  });
+
+
+
