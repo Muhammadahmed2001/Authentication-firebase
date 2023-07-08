763b593b402f36298656011a575395a4184ea5af
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-07-08 15:58:52 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyDuOrp8JQixQzRW9yojXIWP1EF8U74uCBk",
   authDomain: "authentication-firebase-e23fb.firebaseapp.com",
@@ -10,60 +10,72 @@ const firebaseConfig = {
   messagingSenderId: "580457386780",
   appId: "1:580457386780:web:a63da704563d16a3bce0d9"
 };
-
-
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 
+
+let homeBtn = document.getElementById("homeBtn");
+
+
+
+
+
+
+let signUp = document.getElementById("signUp");
+let creatUser = document.getElementById("creatUser")
+creatUser.addEventListener("click", () => {
+  
+  signUp.style.display = "block";
+  homeBtn.style.display = "none"
+})
+
+
+let signUpEmail = document.getElementById("signUpEmail")
+let signUpPassword = document.getElementById("signUpPassword")
+let singUpBtnForCreatUser = document.getElementById("singUpBtn")
+singUpBtnForCreatUser.addEventListener("click", () => {
+  createUserWithEmailAndPassword(auth, signUpEmail.value, signUpPassword.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user + "user")
+      
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage + "err");
+      
+    })
+
+});
 
 
 
 
 
 let login = document.getElementById("login");
-let getButton = document.getElementById("getButton")
-getButton.addEventListener("click", () => {
+let logInUser = document.getElementById("logInUser")
+logInUser.addEventListener("click", ()=>{
+  login.style.display = "block";
+  homeBtn.style.display = "none"
 
-  login.innerHTML = `  <div class="form-box">
-  <form class="form">
-      <span class="title">Sign up</span>
-      <span class="subtitle">Create a free account with your email.</span>
-      <div class="form-container">
-          <input id="signUpEmail" type="email" class="input" value="" placeholder="Email">
-          <input id="signUpPassword" type="password" class="input" placeholder="Password">
-      </div>
-      <button id="singUpBtn">Sign up</button>
-  </form>
-  <!-- <div class="form-section">
-        <p>Have an account? <a href="signIn.html">Log in</a> </p>
-        </div> -->
-</div>
-            
-    `
-    getButton.style.display = "none"
-  })
-    
+})
 
 
 
-
-let addUser = document.getElementById("addUser")
-
-addUser.addEventListener("click",()=>{
-  let signUpEmail = document.getElementById("signUpEmail")
-  let signUpPassword = document.getElementById("signUpPassword")
-  createUserWithEmailAndPassword(auth, signUpEmail.value, signUpPassword.value)
+let signInEmail = document.getElementById("signInEmail")
+let signInPassword = document.getElementById("signInPassword")
+let logInBtn = document.getElementById("logInBtn");
+logInBtn.addEventListener("click", () => {
+  signInWithEmailAndPassword(auth, signInEmail.value, signInPassword.value)
     .then((userCredential) => {
       const user = userCredential.user;
-      console.log(user)
+      console.log(user + "done")
 
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.log(errorMessage);
-
-    })
-
-});
+    });
+})
